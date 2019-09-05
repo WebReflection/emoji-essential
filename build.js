3f24b39993c8830c18ea50ad5e44d5af825ad62c
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-09-05 19:49:01 +0200

    fixed issue with sub groups

--- build.js
+++ build.js
@@ -14,30 +14,33 @@ app.on('ready', () => {
     win.webContents.executeJavaScript(
       `(() => {
         const emoji = {};
+        const mediumhead = (subgroups, tr) => {
+          const name = tr.firstElementChild.textContent.trim();
+          const list = subgroups[name] || (subgroups[name] = {});
+          while (tr = tr.nextElementSibling) {
+            const {classList} = tr.firstElementChild;
+            if (classList.contains('bighead'))
+              return;
+            const name = tr.querySelector('td.name');
+            if (name) {
+              const code = tr.querySelector('td.code');
+              if (code) {
+                const text = code.textContent.trim();
+                const unicode = text.replace(/U\\+/g, '0x').replace(/[\\s\\n\\r\\f]+/g, ', ');
+                const key = eval('String.fromCodePoint(' + unicode + ')');
+                list[key] = name.textContent.trim();
+              }
+            }
+          }
+        };
         document.querySelectorAll('th.bighead').forEach(th => {
           const group = th.textContent.trim();
           const subgroups = emoji[group] || (emoji[group] = {});
           let tr = th.parentElement;
           while (tr && (tr = tr.nextElementSibling)) {
             const {classList} = tr.firstElementChild;
-            if (classList.contains('mediumhead')) {
-              const name = tr.firstElementChild.textContent.trim();
-              const list = subgroups[name] || (subgroups[name] = {});
-              while (tr = tr.nextElementSibling) {
-                if (tr.firstElementChild.classList.contains('bighead'))
-                  return;
-                const name = tr.querySelector('td.name');
-                if (name) {
-                  const code = tr.querySelector('td.code');
-                  if (code) {
-                    const text = code.textContent.trim();
-                    const unicode = text.replace(/U\\+/g, '0x').replace(/[\\s\\n\\r\\f]+/g, ', ');
-                    const key = eval('String.fromCodePoint(' + unicode + ')');
-                    list[key] = name.textContent.trim();
-                  }
-                }
-              }
-            }
+            if (classList.contains('mediumhead'))
+              mediumhead(subgroups, tr);
             else if (classList.contains('bighead'))
               return;
           }
