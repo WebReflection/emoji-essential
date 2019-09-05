09f1d30156dc4752d84e3ee5d9ad220d0c8a4c33
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-09-05 11:27:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# emoji-essential
+
+An emoji dictionary directly from https://unicode.org/ updated through automation on each unicode release.
+
+The dictionary contains groups as main key, sub group per each main key, and finally an emoji > short name key/value pair.
+
+Suitable for most common emoji related operations.
+
+```js
+import emojiEssential from 'emoji-essential';
+// or
+const emojiEssential = require('emoji-essential');
+
+console.log(emojiEssential.Activities.event[`🎉`]);
+"party popper"
+```
+
+
+## Handy variants
+
+This section contains few ways to transform current module for you needs.
+
+The idea is to use one base that contains all the latest unicode code, and create different versions of the same base with the smallest amount of code.
+
+
+#### :emoji_name: to emoji and vice-versa
+
+Useful for markdown cases or forums.
+
+```js
+const ee = require('emoji-essential');
+const name2emoji = {};
+Object.keys(ee).forEach(group => {
+  Object.keys(ee[group]).forEach(sub => {
+    Object.keys(ee[group][sub]).forEach(emoji => {
+      const key = `:${ee[group][sub][emoji].replace(/[ :]+/g, '_')}:`;
+      name2emoji[key] = emoji;
+      name2emoji[emoji] = key;
+    });
+  });
+});
+
+name2emoji[':party_popper:']; // 🎉
+name2emoji['🎉']; // :party_popper:
+```
---
+++ build.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const {app, BrowserWindow} = require('electron');
+
+app.on('ready', () => {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    images: false,
+    webgl: false
+  });
+  win.loadURL('https://unicode.org/emoji/charts/full-emoji-list.html');
+  console.log('please wait');
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.executeJavaScript(
+      `(() => {
+        const emoji = {};
+        document.querySelectorAll('th.bighead').forEach(th => {
+          const group = th.textContent.trim();
+          const subgroups = emoji[group] || (emoji[group] = {});
+          let tr = th.parentElement;
+          while (tr && (tr = tr.nextElementSibling)) {
+            const {classList} = tr.firstElementChild;
+            if (classList.contains('mediumhead')) {
+              const name = tr.firstElementChild.textContent.trim();
+              const list = subgroups[name] || (subgroups[name] = {});
+              while (tr = tr.nextElementSibling) {
+                if (tr.firstElementChild.classList.contains('bighead'))
+                  return;
+                const name = tr.querySelector('td.name');
+                if (name) {
+                  const code = tr.querySelector('td.code');
+                  if (code) {
+                    const text = code.textContent.trim();
+                    const unicode = text.replace(/U\\+/g, '0x').replace(/[\\s\\n\\r\\f]+/g, ', ');
+                    const key = eval('String.fromCodePoint(' + unicode + ')');
+                    list[key] = name.textContent.trim();
+                  }
+                }
+              }
+            }
+            else if (classList.contains('bighead'))
+              return;
+          }
+        });
+        return emoji;
+      })()`,
+      emoji => {
+        fs.writeFile(
+          'index.js',
+          `var emojiEssential = ${JSON.stringify(emoji, null, '  ')};\n`,
+          err => {
+            if (err) {
+              console.error(err);
+              process.exit(1);
+            }
+            win.close();
+          }
+        );
+      }
+    );
+  });
+});
